497d4e16e25352a6d4ace49055c36e3bdb8e9b31
Mai Huynh Loc <kevin.hung@evvoiot.com>
2023-06-15 17:03:10 +0700

    Feat(Auth): Build UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    loadComponent: () => import("./core/auth/auth.component").then((m) => m.AuthComponent)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
